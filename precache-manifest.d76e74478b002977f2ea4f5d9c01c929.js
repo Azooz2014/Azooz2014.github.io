7e51c8d57d4a6f358457db151cbf9907cae43d81
Azooz2014 <Azooz2014@users.noreply.github.com>
2021-08-30 12:29:41 +0000

    Deploying to gh-pages from  @ 032c440dca827a1cae9e57c5f963da6fc32bf124 🚀

---
+++ precache-manifest.d76e74478b002977f2ea4f5d9c01c929.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e189a09b6e65eae72e5d8bf4fce06ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b49cb7d2544700ae8d7",
+    "url": "/static/css/3.d29f9f24.chunk.css"
+  },
+  {
+    "revision": "87df72940db7bd65967e",
+    "url": "/static/css/4.7369bb08.chunk.css"
+  },
+  {
+    "revision": "4124f9606e1cfb571f2d",
+    "url": "/static/css/main.6e600604.chunk.css"
+  },
+  {
+    "revision": "bde1b573070cdcf5fb35",
+    "url": "/static/js/2.761daa7b.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.761daa7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b49cb7d2544700ae8d7",
+    "url": "/static/js/3.46e1473e.chunk.js"
+  },
+  {
+    "revision": "87df72940db7bd65967e",
+    "url": "/static/js/4.a6d8a92d.chunk.js"
+  },
+  {
+    "revision": "4124f9606e1cfb571f2d",
+    "url": "/static/js/main.c18bf080.chunk.js"
+  },
+  {
+    "revision": "6fbb715dca81b1816e1b",
+    "url": "/static/js/runtime-main.0a19b881.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e89ee20f92eff7b572bc0c16b38923fd",
+    "url": "/static/media/fenda-logo.e89ee20f.png"
+  },
+  {
+    "revision": "2cc500ee01eb896efd46a5bc3e5366b0",
+    "url": "/static/media/google.2cc500ee.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "7620e536942a5d589980c73c15664dfa",
+    "url": "/static/media/osmania-logo.7620e536.jpg"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
